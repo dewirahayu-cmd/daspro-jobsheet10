fe0faf8602c53a8d52f7b1c548a3e917e0482dda
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2025-11-21 22:34:16 +0700

    modifikasi 3

--- bioskop08.java
+++ bioskop08.java
@@ -12,10 +12,10 @@ public class bioskop08 {
         penonton[3][1] = "Hana";
 
         System.out.println(penonton.length);
-        System.out.println(penonton[0].length);
-        System.out.println(penonton[1].length);
-        System.out.println(penonton[2].length);
-        System.out.println(penonton[3].length);
+        
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton[i].length);
+        }
 
     }
 }
