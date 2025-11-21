eaa76b11cf7b96a9868f6c1d638ef16b87bfc8fb
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2025-11-22 01:02:39 +0700

    modifikasi 3

--- SIAKAD08.java
+++ SIAKAD08.java
@@ -4,7 +4,12 @@ public class SIAKAD08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int [][] nilai = new int[4][3];
+        System.out.println("Masukkan jumlah mahasiswa: ");
+        int n = sc.nextInt();
+        System.out.println("Masukkan jumlah mata kuliah: ");
+        int m = sc.nextInt();
+
+        int [][] nilai = new int[n][m];
 
         for (int i = 0; i < nilai.length; i++){
             System.out.println("Input nilai mahasiswa ke-" + (i+1));
@@ -15,8 +20,19 @@ public class SIAKAD08 {
                 nilai[i][j] = sc.nextInt();
                 totalPersiswa += nilai[i][j]; 
             }
-            System.out.println("Nilai rata-rata: " + totalPersiswa/3);
+            System.out.println("Nilai rata-rata: " + totalPersiswa/m);
         }
+        System.out.println("\n===================================");
+        System.out.println("Rata-rata nilai setiap Mata Kuliah: ");
+
+        for (int j = 0; j < m; j++){
+            double totalPermatkul = 0;
+            for (int i = 0; i < n; i++){
+                totalPermatkul += nilai[i][j];
+            }
+            System.out.println("Mata kuliah " + (j + 1) + ": " + totalPermatkul / n);
+        }
+
         sc.close();
     }
 }
